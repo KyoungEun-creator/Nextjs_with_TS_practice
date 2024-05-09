c8aa3198e5a6d001e40e491806a78814c7dfec79
조경은 <kavvy99@naver.com>
2024-05-09 15:05:57 +0900

    #3.3: add loading component which makes browser wait for data fetching from server side with showing rest of UI

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,12 +5,12 @@ export const metadata = {
 const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  return fetch(URL).then((response) => response.json());
+  // 백엔드로부터의 전달을 일부러 늦추기 위한 장치
+  await new Promise((resolve) => setTimeout(resolve, 10000));
 
-  // const response = await fetch(URL);
-  // const json = await response.json();
-  // return json;
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
 }
 export default async function HomePage() {
   const movies = await getMovies();
