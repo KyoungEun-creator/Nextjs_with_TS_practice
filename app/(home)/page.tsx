1e5676ecca08b5d1c41c64a83ba6ca6a1740bd31
조경은 <kavvy99@naver.com>
2024-05-09 17:58:13 +0900

    #3.4: add mapping api data into /movies/:id with loading page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,18 +1,28 @@
+import Link from "next/link";
+
 export const metadata = {
   title: "Home",
 };
 
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
-  // 백엔드로부터의 전달을 일부러 늦추기 위한 장치
-  await new Promise((resolve) => setTimeout(resolve, 10000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
 
-  const response = await fetch(URL);
+  const response = await fetch(API_URL);
   const json = await response.json();
   return json;
 }
+
 export default async function HomePage() {
   const movies = await getMovies();
-  return <div>{JSON.stringify(movies)}</div>;
+  return (
+    <div>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </div>
+  );
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { API_URL } from "../../../(home)/page";
 
-export default function MovieDetail({ params: { id } }: { params: { id: string } }) {
-  return <h1>Movie {id}</h1>;
+async function getMovie(id: string) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
 }
 
 // { params: { id } }: { params: { id: string } }
 // { searchParams: { region } }: { searchParams: { region: string } }
+
+export default async function MovieDetail({ params: { id } }: { params: { id: string } }) {
+  const movie = await getMovie(id);
+  console.log(movie);
+  return <h1>{movie.title}</h1>;
+}
