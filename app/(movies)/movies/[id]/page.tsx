8c8f4d23bea18d9bcfda6a73d8dcb75f8cc5b55a
조경은 <kavvy99@naver.com>
2024-05-09 18:10:25 +0900

    #3.4: check problem of adding two data fetchings

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,8 +2,16 @@ import React from "react";
 import { API_URL } from "../../../(home)/page";
 
 async function getMovie(id: string) {
+  console.log(`Fetching movies: ${Date.now()}`);
   await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
+}
+
+async function getVideos(id: string) {
+  console.log(`Fetching videos: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(`${API_URL}/${id}/videos`);
   return response.json();
 }
 
@@ -11,7 +19,14 @@ async function getMovie(id: string) {
 // { searchParams: { region } }: { searchParams: { region: string } }
 
 export default async function MovieDetail({ params: { id } }: { params: { id: string } }) {
+  console.log("start fetching");
   const movie = await getMovie(id);
-  console.log(movie);
-  return <h1>{movie.title}</h1>;
+  const videos = await getVideos(id);
+  console.log("end fetching");
+
+  return (
+    <>
+      <h1>{movie.title}</h1>
+    </>
+  );
 }
