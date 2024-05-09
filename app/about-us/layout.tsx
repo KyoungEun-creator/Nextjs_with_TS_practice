1e1f35d238b90b59086d41a7dc452f5d93fbdb77
조경은 <kavvy99@naver.com>
2024-05-09 11:35:28 +0900

    #2.7: add navigation inside of layout file which can be nested between pages with same parent URL

---
+++ app/about-us/layout.tsx
@@ -0,0 +1,8 @@
+export default function AboutUsLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      {children}
+      &copy; This is a Layout only for AboutUS page! How cool!
+    </div>
+  );
+}
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,22 +1,22 @@
-"use client"
+"use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation"
+import { usePathname } from "next/navigation";
 
 export default function Navigation() {
-    const path = usePathname();
-    console.log(path)
+  const path = usePathname();
+  console.log(path);
 
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <Link href="/">Home</Link> {path === "/" ? "🌱" : ""}
-                </li>
-                <li>
-                    <Link href="/about-us">About Us</Link> {path === "/about-us" ? "🌱" : ""}
-                </li>
-            </ul>
-        </nav>
-    )
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">Home</Link> {path === "/" ? "🌱" : ""}
+        </li>
+        <li>
+          <Link href="/about-us">About Us</Link> {path === "/about-us" ? "🌱" : ""}
+        </li>
+      </ul>
+    </nav>
+  );
 }
