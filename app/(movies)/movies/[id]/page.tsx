0a795ae7f51bfa28bae22dac44c3b47358307e71
조경은 <kavvy99@naver.com>
2024-05-09 12:38:55 +0900

    #2.9: dynamic routes using folder named with parentheses

---
+++ app/(movies)/movies/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function MovieDetail({ params: { id } }: { params: { id: string } }) {
+  //   console.log(props);
+  return <h1>Movie {id}</h1>;
+}
+
+// { params: { id } }: { params: { id: string } }
+// { searchParams: { region } }: { searchParams: { region: string } }
